0bc721a5040b52c6eae6105b5aa43ddd7eeeb275
Steven <steven.marga@gmail.com>
2019-11-13 14:53:11 +0200

    Updates

---
+++ precache-manifest.65ce6d513225ce739f9ae47c5ccb89ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b55022f467a5141154a5feca7641fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c148b07c4e47c1137c1",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ac00b2b756fb55db245d",
+    "url": "/static/css/main.c2c1831c.chunk.css"
+  },
+  {
+    "revision": "2c148b07c4e47c1137c1",
+    "url": "/static/js/2.696ee197.chunk.js"
+  },
+  {
+    "revision": "ac00b2b756fb55db245d",
+    "url": "/static/js/main.d6eb58a7.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
