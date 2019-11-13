05a8e6c4a4feed61906c87c4d265a2d59747f8c1
Steven <steven.marga@gmail.com>
2019-11-13 14:59:34 +0200

    remove error example

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error("Noooo!");
-  }
+  // if (true) {
+  //   throw new Error("Noooo!");
+  // }
   return (
     <div>
       {robots.map((user, i) => {
